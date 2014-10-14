5501165f9d14cae5ccce40859fe6c0b79addd352
jose antonio <ellocodelgorro@gmail.com>
2014-10-14 22:35:24 +0200

    OrgHeiglHybridAuth

--- config/application.config.php
+++ config/application.config.php
@@ -5,7 +5,8 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
-	'Database'
+	'Database',
+        'OrgHeiglHybridAuth'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
